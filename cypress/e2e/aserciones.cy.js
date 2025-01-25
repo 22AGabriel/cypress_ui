f3b012adb26b40297f35707631ecf5bdb0880ab4
Gabriel Azubel <gabrielazubel63t@gmail.com>
2025-01-24 23:00:13 -0300

    Hooks

--- cypress/e2e/aserciones.cy.js
+++ cypress/e2e/aserciones.cy.js
@@ -1,22 +1,35 @@
 describe('Aserciones', () => {
 
-    it('Aserción', () => {
-        cy.visit('/automation-practice-form')
+    // before(() => {
+    //     cy.visit('/automation-practice-form')
+    // })
 
+    beforeEach(() => {
+        cy.visit('/automation-practice-form')
+    })
+
+    // after(() => {
+    //     cy.visit('https://www.google.com')
+    // })
+
+    afterEach(() => {
+        cy.visit('https://www.google.com')
+        cy.wait(2000)
+    })
+
+    it('Aserción', () => {
         cy.url().should('include', 'demoqa.com')
         cy.get('#firstName').should('be.visible').and('have.attr', 'placeholder', 'First Name')
     })
 
     it('Aserción 2', () => {
-        cy.visit('/automation-practice-form')
         cy.get('#firstName').then((element) => {
             expect(element).to.be.visible
             expect(element).to.have.attr('placeholder', 'First Name')
         })
     })
 
-    it.only('Aserción 3', () => {
-        cy.visit('/automation-practice-form')
+    it('Aserción 3', () => {
         cy.get('#firstName').then((element) => {
             assert.equal(element.attr('placeholder'), 'First Name')
         })
