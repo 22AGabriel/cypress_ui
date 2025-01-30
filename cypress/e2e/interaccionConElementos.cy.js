5354d6bd915a012abc84e29e2b119a266bd3fb9f
Gabriel Azubel <gabrielazubel63t@gmail.com>
2025-01-29 23:28:17 -0300

    Improving form interactions

--- cypress/e2e/interaccionConElementos.cy.js
+++ cypress/e2e/interaccionConElementos.cy.js
@@ -23,9 +23,35 @@ describe('Interacción con elementos', () => {
         cy.get('#enableAfter').click({force: true})
     })
 
-    it.only('Click por posición', () => {
+    it('Click por posición', () => {
         cy.visit(('/buttons'))
         cy.get('button').eq(3).parent().parent().click('topRight')
         cy.get('button').eq(3).parent().parent().click(50, 50)
+    })
+
+    it('Input de tipo texto', () => {
+        cy.visit('/automation-practice-form')
+        cy.get('#firstName').type('Gabriel')
+        cy.get('#lastName').type('Azubel')
+
+        cy.get('#firstName').type('Gabriel') // Se concatena
+
+        cy.get('#firstName').clear() // Mejor opcion
+        // cy.get('#firstName').type('{selectAll}{backspace}') Simula una interacción más cercana a la del usuario real.
+
+        cy.get('#firstName').type('Otro nombre')
+    })
+
+    it.only('Checkboxes y radio buttons', () => {
+        cy.visit('/automation-practice-form')
+
+        // cy.get('#gender-radio-1').check() // Error al estar envuelto en un label
+        // cy.get('#gender-radio-1').check({force: true}) // Comportamiento no optimo
+
+        cy.get('label[for="gender-radio-1"]').click()
+
+        // cy.get('#hobbies-checkbox-1').check({force: true})
+        // cy.get('#hobbies-checkbox-1').uncheck({force: true})
+        cy.get('label[for="hobbies-checkbox-1"]').click()
     })
 })
